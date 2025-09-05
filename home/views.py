9db9300aa0d753f34ee6fd7256f9da6897c49e72
junseok <rhwnstjr23@naver.com>
2025-09-05 12:42:24 +0900

    18nd

--- home/views.py
+++ home/views.py
@@ -1,33 +1,49 @@
+# home/views.py
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.db.models import Count
 from board.models import WBSItem
 
 @login_required
 def home(request):
-    # 1) 로그인 유저의 할당된 WBS 개수
-    #my_count = WBSItem.objects.filter(task_owner=request.user.name).count()
-
-    # 2) 전체 WBS 개수 & 상태별 개수
-    #total_count = WBSItem.objects.count()
-    #progress_counts = (
-    #    WBSItem.objects
-    #           .values('progress')
-    #           .order_by()
-    #           .annotate(count=models.Count('progress'))
-    #)
-    # 편의상 dict 형태로
-    #prog_dict = {d['progress']: d['count'] for d in progress_counts}
-
-    # 3) In progress 상태의 최근 10개 항목
+    # In progress 최근 10개
     in_progress_items = (
         WBSItem.objects
                .filter(progress='In progress')
                .order_by('-no')[:10]
     )
 
-    return render(request, 'home.html', {
-        #'my_count': my_count,
-        #'total_count': total_count,
-        #'prog_counts': prog_dict,
+    wbs_qs = WBSItem.objects.all()
+
+    # 전체 개수
+    wbs_total = wbs_qs.count()
+
+    # 상태별 개수 (기본값 0으로 세팅 후 채움)
+    key_map = {
+        'In progress': 'in_progress',
+        'COMPLETE': 'complete',
+        'Wait': 'wait',
+        'Hold': 'hold',
+    }
+    progress_counts = {'in_progress': 0, 'complete': 0, 'wait': 0, 'hold': 0}
+    for row in wbs_qs.values('progress').annotate(cnt=Count('id')):
+        k = (row['progress'] or '').strip()
+        if k in key_map:
+            progress_counts[key_map[k]] = row['cnt']
+
+    # Task Owner TOP 5 (빈값/’nan’ 제외)
+    owner_top5 = (
+        wbs_qs.exclude(task_owner__isnull=True)
+              .exclude(task_owner__in=['', 'nan', 'NaN'])
+              .values('task_owner')
+              .annotate(cnt=Count('id'))
+              .order_by('-cnt')[:5]
+    )
+
+    ctx = {
+        'wbs_total': wbs_total,
+        'progress_counts': progress_counts,
+        'owner_top5': owner_top5,
         'in_progress_items': in_progress_items,
-    })
+    }
+    return render(request, 'home.html', ctx)
